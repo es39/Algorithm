d1bf080ade81967b1d943f8722b94e904449ff5f
es39 <115976203+es39@users.noreply.github.com>
2023-01-13 19:16:02 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,12 +1,16 @@
+// function solution(my_string) {
+//     let str = my_string
+//     let result = ''
+//     for(let i=0; i<my_string.length; i++){
+//         if(str[i].toLowerCase() === my_string[i]){
+//             result += my_string[i].toUpperCase()
+//         } else {
+//             result += my_string[i].toLowerCase()
+//         }
+//     }
+//     return result
+// }
+
 function solution(my_string) {
-    let str = my_string
-    let result = ''
-    for(let i=0; i<my_string.length; i++){
-        if(str[i].toLowerCase() === my_string[i]){
-            result += my_string[i].toUpperCase()
-        } else {
-            result += my_string[i].toLowerCase()
-        }
-    }
-    return result
+    return my_string.split('').map(el => el === el.toUpperCase() ? el.toLowerCase() : el.toUpperCase()).join('')
 }

4795d1bb935e15edf0fdcc6adad40b1df6a53ee7
es39 <115976203+es39@users.noreply.github.com>
2023-01-13 18:24:21 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,9 +1,4 @@
 function solution(s1, s2) {
-    let count = 0
-    for(let i=0; i<s1.length; i++){
-        for(let j=0; j<s2.length; j++){
-            if(s1[i] === s2[j]) count ++
-        }
-    }
-    return count
+    const result = s1.filter(el => s2.includes(el))
+    return result.length
 }

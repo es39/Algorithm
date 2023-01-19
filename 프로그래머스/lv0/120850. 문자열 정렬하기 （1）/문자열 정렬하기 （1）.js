4b1f19b827cf7452154496c24ecc7ef25fe4f1e5
es39 <115976203+es39@users.noreply.github.com>
2023-01-19 15:34:37 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,8 +1,7 @@
 function solution(my_string) {
-    let result = [...my_string].map(el => parseInt(el))
-    let nums = []
-    for(let i=0; i<result.length; i++){
-        if(!isNaN(result[i])) nums.push(result[i])
-    }
-    return nums.sort((a,b) => a-b)
+return [...my_string].map(el => parseInt(el)).filter(el => !isNaN(el)).sort((a, b) => a-b)
+    // for(let i=0; i<result.length; i++){
+    //     if(!isNaN(result[i])) nums.push(result[i])
+    // }
+    // return nums.sort((a,b) => a-b)
 }

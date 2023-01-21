c096be69cbf217c5110321d96af8b1139b6751f0
es39 <115976203+es39@users.noreply.github.com>
2023-01-21 19:08:14 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,3 +1,4 @@
 function solution(box, n) {
-    return parseInt(box[0]/n) * parseInt(box[1]/n) * parseInt(box[2]/n)
+    // return parseInt(box[0]/n) * parseInt(box[1]/n) * parseInt(box[2]/n)
+    return box.reduce((acc, cur) => acc*parseInt(cur/n), 1)
 }

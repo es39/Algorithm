3147c87bbfb9770cb8341204cf7dd59c03d89925
es39 <115976203+es39@users.noreply.github.com>
2023-01-25 21:37:59 +0900

    [level 0] Title: 소인수분해, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -1,13 +1,17 @@
 function solution(n) {
     // 결과 배열 선언
     let prime = []
-    for(let i=2; i<=n; i++){
+    // 반복문을 통해 소수 판별
+    for(let i=2; i<=Math.sqrt(n); i++){
         while(n%i === 0){
             prime.push(i)
             n /= i;
         }
     }
+    // 최종적으로 나온 몫도 인수로 포함되기 때문에 push
     if(n >= 2) prime.push(n)
+    // set 객체를 이용해 중복을 제거한 후 얕은 복사
     let arr = Array.from(new Set(prime))
+    // 오름차순 정렬
     return arr.sort((a, b) => a-b)
 }

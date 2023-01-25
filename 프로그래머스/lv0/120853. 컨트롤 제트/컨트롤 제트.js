c7f2658b700bc98d4a29d6702c0a59ef68d210a3
es39 <115976203+es39@users.noreply.github.com>
2023-01-25 20:52:41 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,7 +1,11 @@
 function solution(s) {
+    // 공백기준으로 문자열 나눠줌
     let str = s.split(' ')
+    // 반복문을 통해 Z 포함여부 확인
     while(str.includes('Z')){
+        // 포함이 되어 있다면 이전 요소의 인덱스부터 2개 자르기
         str.splice(str.indexOf('Z')-1, 2)
     }
+    // 합
     return str.reduce((acc, cur) => Number(acc)+Number(cur), 0)
 }

bead4ee2632643b9a98dc6f18c1b390b593d9687
es39 <115976203+es39@users.noreply.github.com>
2023-01-31 21:33:34 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,21 +1,22 @@
-// function solution(numbers) {
-//     let digit = ['zero', 'one', 'two', 'three', 'four', 
-//                  'five', 'six', 'seven', 'eight', 'nine']
-//     for(let i=0; i<digit.length; i++){
-//         numbers = numbers.split(digit[i]).join(i)
-//     }
-//     return Number(numbers)
-// }
-
 function solution(numbers) {
-    const obj = {
-        zero: 0, one: 1, two: 2, three: 3, four: 4,
-        five: 5, six: 6, seven: 7, eight: 8, nine: 9
-    };
-
-    const num = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => {
-        return obj[v];
-    });
-
-    return Number(num);
+    // let nums = numbers.split('')
+    let digit = ['zero', 'one', 'two', 'three', 'four', 
+                 'five', 'six', 'seven', 'eight', 'nine']
+    for(let i=0; i<digit.length; i++){
+       numbers = numbers.split(digit[i]).join(i)
+    }
+    return Number(numbers)
 }
+
+// function solution(numbers) {
+//     const obj = {
+//         zero: 0, one: 1, two: 2, three: 3, four: 4,
+//         five: 5, six: 6, seven: 7, eight: 8, nine: 9
+//     };
+
+//     const num = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => {
+//         return obj[v];
+//     });
+
+//     return Number(num);
+// }

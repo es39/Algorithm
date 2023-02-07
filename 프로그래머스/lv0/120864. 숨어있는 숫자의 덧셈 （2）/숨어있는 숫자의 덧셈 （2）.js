2bb104cd8b7a4229b9d1c8e9500b7f462fbbc8aa
es39 <115976203+es39@users.noreply.github.com>
2023-02-07 20:24:57 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    // 1. 문자로 하나씩 나누기
+    // 2. 문자를 순회하여 Number 타입으로 변환
+    // 3. isNaN()과 을 사용하여 문자인지 숫자인지 구분
+    // 4. 숫자라면 각 요소는 string 타입이니 +로 붙여줌
+    // 5. 각각 숫자로 다시 변환하여 계산
+    /* 정규식을 활용해보자 */
+    let nums = my_string.match(/[0-9]+/g)
+    if(nums === null) return 0
+    return nums.reduce((acc, cur) => Number(acc)+ Number(cur), 0)
+}

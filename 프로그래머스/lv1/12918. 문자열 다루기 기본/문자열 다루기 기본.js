1d2e0b26d5f6539d90f120f014ed754e0196ca02
es39 <115976203+es39@users.noreply.github.com>
2023-02-11 13:35:39 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,12 +1,17 @@
-function solution(s) {
-    if(s.length === 4 || s.length === 6){
-        let str = [...s].filter(el => !isNaN(el))
-        if(str.length === s.length){
-            return true
-        } else {
-            return false
-        }
-    } else {
-        return false
-    }
+// function solution1(s) {
+//     if(s.length === 4 || s.length === 6){
+//         let str = [...s].filter(el => !isNaN(el))
+//         if(str.length === s.length){
+//             return true
+//         } else {
+//             return false
+//         }
+//     } else {
+//         return false
+//     }
+// }
+
+function solution(s){
+    var regex = /^\d{6}$|^\d{4}$/;
+    return regex.test(s);
 }

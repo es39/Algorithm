a80a7cdb1363b3a2803a26f48cb47c90c4746cfa
es39 <115976203+es39@users.noreply.github.com>
2023-02-14 21:14:47 +0900

    [level 2] Title: 피보나치 수, Time: 4.50 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -3,7 +3,9 @@ function solution(n) {
     for(let i=2; i<=n; i++){
         const n1 = fibo[i-1]
         const n2 = fibo[i-2]
+        //fibo.push(n1+n2)
         fibo.push((n1+n2)%1234567)
     }
     return fibo[n]
+    // return fibo[n]%1234567
 }

135539fdc6ef24373c3384cb07929f230beadd2b
es39 <115976203+es39@users.noreply.github.com>
2023-02-14 20:09:45 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,7 @@
+function solution(s) {
+    let num = s.split(' ').map(el => Number(el))
+    let max = Math.max(...num)
+    let min = Math.min(...num)
+    let result = min+""+" "+max
+    return result
+}

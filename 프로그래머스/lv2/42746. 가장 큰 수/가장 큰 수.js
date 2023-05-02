3bb8a4012a79c23ad8a32df2e3e6e0698ddcaee8
es39 <esj10025@gmail.com>
2023-05-02 15:10:29 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,5 +1,8 @@
 function solution(numbers) {
-    let nums = numbers.map(el => el+'').sort((a, b) => (b+a) - (a+b))
+    // 1. 이중반복문?
+    // 2. 문자열 변환 -> sort 로 비교하여 정렬
+    let nums = numbers.map(el => el+'').sort((a, b) => (b+a) - (a+b)) 
+    // '6', '10' -> '610' - '106' 
     if(nums[0] === '0') return '0'
     return nums.join('')
 }

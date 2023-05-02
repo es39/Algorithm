73b429c1a590b3dfdb59d2bcbfc972863fd01852
es39 <esj10025@gmail.com>
2023-05-02 14:14:48 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,20 +1,20 @@
-const gcd = (a, b) => {
-    while(b > 0) {
-        let r = b
-        b = a % b;
-        a = r
-    }
-    return a
-}
-
-const lcm = (a, b) => {
-    return (a * b) / gcd(a, b)
-}
-
 function solution(arr) {
     let result = 1;
     for(let i=0; i<arr.length; i++){
-        result = lcm(result, arr[i])
+        result = lcm(arr[i], result)
     }
     return result
+}
+
+function gcd(a, b) {
+    while(b > 0){
+        let r = b;
+        b = a % b;
+        a = r
+    }
+    return a;
+}
+
+function lcm(a, b) {
+    return (a * b) / gcd(a, b)
 }

e2dbea03d663b89607b310ff347e398ecf68327e
es39 <esj10025@gmail.com>
2023-05-02 14:33:25 +0900

    [level 2] Title: 올바른 괄호, Time: 2.78 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,9 @@
+function solution(s){
+    let stack = 0;
+    if(s.length === 1 || s[0] === ")") return false;
+    for(let i=0; i<s.length; i++){
+        s[i] == '(' ? stack++ : stack --;
+        if(stack < 0) return false
+    }
+    return stack == 0 ? true : false;
+}

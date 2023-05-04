5a523b6071f5d1e099f7cf0f7b187d2d90017a45
es39 <esj10025@gmail.com>
2023-05-04 13:55:12 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,7 +1,7 @@
 function solution(n) {
     let fibo = [0, 1, 2]
     for(let i=3; i<=n; i++){
-        fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567
+        fibo[i] = (fibo[i-2]+fibo[i-1]) % 1234567
     }
     return fibo[n]
 }

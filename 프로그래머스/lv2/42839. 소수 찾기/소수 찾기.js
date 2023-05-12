90a38c488218b940d5c4c708dbe95878f129bcb2
es39 <esj10025@gmail.com>
2023-05-12 14:15:35 +0900

    [level 2] Title: 소수 찾기, Time: 0.44 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,30 @@
+// 소수 카운트
+const isPrime = (n) => {
+    if(n < 2) return false
+    if(n === 2) return true
+    for(let i=2; i<=Math.sqrt(n); i++){
+        if(n % i === 0) return false
+    }
+    return true
+}
+
+function solution(numbers) {
+    // 1. 숫자 -> 배열
+    // 2. 순열 
+    // 3. 소수 카운트
+    let nums = numbers.split('').map(el => Number(el))
+    let result = new Set()
+    // 순열 만들기
+    const getPermutation = (arr, fixed) => {
+        for(let i=0; i<arr.length; i++){
+            const newArr = [...arr]
+            newArr.splice(i, 1)
+            if (isPrime(parseInt(fixed + arr[i]))) {
+            result.add(parseInt(fixed + arr[i]));
+            }
+            getPermutation(newArr, fixed + arr[i]);
+        }
+    }
+    getPermutation(nums, '')
+    return result.size
+}

99239a664950cc29812563651bd220b5695f37a9
조은선 <esj10025@gmail.com>
2023-05-15 15:16:44 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -1,10 +1,11 @@
 function solution(n,a,b) {
     // n : 참가자 수
+    // 다음 라운드 번호 N/2
     let result = 0;
     while(a !== b) {
         a = Math.round(a/2)
         b = Math.round(b/2)
-        result ++;
+        result ++
     }
     return result
 }

4095945ea601d14a208002cbe8e772367363b6ab
조은선 <esj10025@gmail.com>
2023-05-15 13:43:00 +0900

    [level 2] Title: 구명보트, Time: 34.00 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,17 +1,21 @@
 function solution(people, limit) {
+    // 1. 정렬
+    // 2. 앞, 뒤 합과 limit 비교
+    // 3. 인덱스 옮기며 result 증가
     let result = 0 // 보트 수
-    let sorted = people.sort((a, b) => a-b)
-    let left = 0
+    people.sort((a, b) => a-b)
+    let left = 0;
     let right = people.length-1
-    while(left<right) {
+    while(left < right) {
         if(people[left] + people[right] > limit) {
             right --
-        } else{
+        } else {
             left ++
-            right --;
+            right --
         }
         result ++;
     }
-    if(left === right) result ++;
+    // 마지막에 1개 남았을 때
+    if(left === right) result ++
     return result
 }

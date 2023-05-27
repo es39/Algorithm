3502b11b3bffaf945459e8dda54b15c56cfa99e7
조은선 <esj10025@gmail.com>
2023-05-27 17:08:32 +0900

    [level 0] Title: 등수 매기기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -2,6 +2,6 @@ function solution(score) {
     // 1. 평균 구하기
     // 2. 등수 매기기
     let avg_arr = score.map(el => (el[0]+el[1])/2)
-    let sorted = avg_arr.slice().sort((a,b)=>b-a);
+    let sorted = avg_arr.slice().sort((a, b) => b-a);
     return avg_arr.map(el => sorted.indexOf(el)+1)
 }

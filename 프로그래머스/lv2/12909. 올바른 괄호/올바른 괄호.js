8cad1613dfb6b3f3c62ad055e226553e080449e4
조은선 <esj10025@gmail.com>
2023-05-29 15:29:09 +0900

    [level 2] Title: 올바른 괄호, Time: 2.59 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,6 +1,6 @@
 function solution(s){
     let stack = 0;
-    if(s.length === 1 || s[0] === ")") return false;
+    // if(s.length === 1 || s[0] === ')') return false;
     for(let i=0; i<s.length; i++){
         s[i] == '(' ? stack++ : stack --;
         if(stack < 0) return false

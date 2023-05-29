e24506dbcbeb64f9df62ef7f5010117c46adaa29
조은선 <esj10025@gmail.com>
2023-05-29 15:12:12 +0900

    [level 2] Title: 프로세스, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.js
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.js
@@ -1,19 +1,25 @@
 function solution(priorities, location) {
     // 우선순위 먼저 파악하기
     let max = Math.max(...priorities);
+    // 위치 (index)
     let position = [];
+    // return 값
     let result = 0;
+    
+    // 위치 저장
     for(let i=0; i<priorities.length; i++){
         position.push(i);
-    }
+    } // [0, 1, 2, 3]
+    // 우선순위 판별
     while(position.length !== 0) {
         if(priorities[0] < max) {
-            priorities.push(priorities.shift());
-            position.push(position.shift());
-        } else {
+            // 뒤로 밀기
+            priorities.push(priorities.shift()); // [1, 3, 2, 2]
+            position.push(position.shift()); // [1, 2, 3, 0]
+        } else { 
             result ++;
-            priorities.shift()
-            if(position.shift() === location) return result
+            priorities.shift() // [3, 2, 2, 1]
+            if(position.shift() === location) return result 
             max = Math.max(...priorities);
         }
     }

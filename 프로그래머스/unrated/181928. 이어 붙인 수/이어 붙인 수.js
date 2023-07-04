2d643e0f5cfcc5f3a7af3adfee434ff4c59c7270
조은선 <esj10025@gmail.com>
2023-07-04 15:29:17 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+    let odd = ''
+    let even = ''
+    for(let i=0; i<num_list.length; i++){
+        if(num_list[i] % 2 !== 0) odd = odd+num_list[i]
+        else even = even+num_list[i]
+    }
+    return Number(odd) + Number(even)
+}
